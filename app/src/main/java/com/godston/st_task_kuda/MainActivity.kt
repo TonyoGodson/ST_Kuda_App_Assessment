a324a439422971f418a4e844b725206311426e86
TonyoGodson <59567558+TonyoGodson@users.noreply.github.com>
2023-06-10 20:12:57 +0100

    nav graph added, all views given id

--- app/src/main/java/com/godston/st_task_kuda/MainActivity.kt
+++ app/src/main/java/com/godston/st_task_kuda/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.godston.st_task_kuda
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Usage example
+        val statusBarColor = ContextCompat.getColor(this, R.color.kuda_purple)
+        setStatusBarColor(this, statusBarColor)
+    }
+
+    // Set the status bar color dynamically
+    fun setStatusBarColor(activity: Activity, color: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = activity.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = color
+        }
     }
 }
